85000cd075f5ce2ce2d7ed189daddbd802f1a84d
kiran sonne <kiransonne04@gmail.com>
2022-11-24 15:16:59 +0530

    Initial Commit

---
+++ CupCakeCornerApp/CupCakeCornerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerAppApp.swift
+//  CupCakeCornerApp
+//
+//  Created by Kiran Sonne on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
